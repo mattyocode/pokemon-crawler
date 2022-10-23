6fe3c7242353a8268cb9babc52d4f8d1b17d78ac
Matt <63904102+mattyocode@users.noreply.github.com>
2022-10-23 21:11:51 +0100

    register models with admin

--- pokemon/admin.py
+++ pokemon/admin.py
@@ -1,3 +1,24 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Pokemon
+
+
+@admin.register(Pokemon)
+class PokemonAdmin(admin.ModelAdmin):
+    fields = (
+        "name",
+        "height",
+        "weight",
+        "stats",
+        "sprite",
+    )
+    list_display = (
+        "name",
+        "height",
+        "weight",
+        "stats",
+        "sprite",
+    )
+
+    def get_abilities(self, instance):
+        return [ability.name for ability in instance.ability.all()]
